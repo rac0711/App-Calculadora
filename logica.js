1f73a84c05ade083f466032f9a31194ecf6ce3b7
Rafael Carvalho <rafael_carvalho07@hotmail.com>
2023-06-18 14:37:59 -0300

    Desenvolvendo a funcionalidade Local Storage
    Lógica de setar o valor de igualdade no localStorage

--- logica.js
+++ logica.js
@@ -1,27 +1,37 @@
 function calcular(tipo, valor){
-    
-    if(tipo === 'operacao'){
+  if(tipo === 'operacao'){
 
-      if(valor === 'c') {
-        //limpar o visor(id resultado)
-        document.getElementById('resultado').value = ''
-          }
-            //concatenar e atribuir o valor do resultado com o símbolo das operações matemáticas
-          if(valor ==='+' || valor === '-' || valor === '*' || valor ==='/' || valor === '.'){
+    if(valor === 'c') {
+      //limpar o visor(id resultado)
+      document.getElementById('resultado').value = ''
+      localStorage.removeItem("igualdade");
+    }
 
-            document.getElementById('resultado').value += valor
-          }
-            // transformando o valor do input resultado '=' como nativo js e setando 
-            //o '=' com o resultado do valor do input 
-          if (valor === '='){
-          var valor_campo = eval (document.getElementById('resultado').value)
+    //concatenar e atribuir o valor do resultado com o símbolo das operações matemáticas
+    if(valor ==='+' || valor === '-' || valor === '*' || valor ==='/' || valor === '.'){
+      document.getElementById('resultado').value += valor
+    }
 
-              document.getElementById('resultado').value = valor_campo
-          }
-      // atribuindo o tipo numérico o valor do input com o valor definido por parâmetro e concatenando
-        } else if(tipo === 'numero') {
-        
-          document.getElementById('resultado').value += valor
+    // transformando o valor do input resultado '=' como nativo js e setando o '=' com o resultado do valor do input 
+    if (valor === '='){
+      // Seta no localstorage o valor true, indicando que o '=' ja foi teclado
+      localStorage.setItem("igualdade", "true");
 
-        }
+      var valor_campo = eval (document.getElementById('resultado').value)
+
+      document.getElementById('resultado').value = valor_campo
+    }
+
+  // atribuindo o tipo numérico o valor do input com o valor definido por parâmetro e concatenando
+  } else if(tipo === 'numero') {
+    // Se o valor é true no localstorage, indica que o igual já foi teclado, ou seja, agora que ele está tentando por um número, o visor vai ser resetado antes disso
+    if(localStorage.getItem("igualdade") == "true"){
+      //limpar o visor
+      document.getElementById('resultado').value = ''
+      // Remove do localstorage 
+      localStorage.removeItem("igualdade");
+    }
+    // Tecla o número
+    document.getElementById('resultado').value += valor
   }
+}
